f4e2fd85d42f0ea1df848d102f7295131a9698f2
Donny Winston <dwinston@alum.mit.edu>
2018-10-09 14:20:31 -0400

    Refactor get_fremont_data for speed

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -23,7 +23,11 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL,
     if force_download or not os.path.exists(filename):
         # You can now download the data from the database
         urlretrieve(url, filename)
-    data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+    data = pd.read_csv(filename, index_col='Date')
+    try:
+        data.index = pd.to_datetime(data.index, format="%m/%d/%Y %H:%M:%S %p")
+    except (ValueError, TypeError):
+        data.index = pd.to_datetime(data.index)
     data.columns = ['East', 'West']
     data['Total'] = data['West'] + data['East']
     return data
